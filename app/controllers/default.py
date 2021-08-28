d0d96dcef585aee728fc1e0ac8c4e3862131e8e5
gabrielmotroni-prog <60718584+gabrielmotroni-prog@users.noreply.github.com>
2021-08-27 21:00:57 -0300

    vs01

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -24,6 +24,31 @@ def index():
 #-------------------------- cliente --------------------------#
 #-------------------------- cliente --------------------------#
 
+@app.route('/clientes')
+def clientes():
+    clientes = Cliente.list_cliente()
+
+    return render_template('clientes.html', clientes=clientes)
+
 @app.route('/cliente/cadastrar')
 def cliente_cadastrar():
     return render_template('cliente_cadastrar.html')
+
+@app.route('/cliente/cadastrar/salvar',methods=['POST'])
+def cliente_cadastrar_salvar():
+    nome_cliente = request.form['nome_cliente']
+    cep_cliente = request.form['cep']
+    rua_cliente = request.form['endereco']
+    bairro_cliente = request.form['bairro']
+    data_nascimento_cliente = request.form['data_nascimento_cliente']
+    CPF_cliente = request.form['CPF_cliente']
+    email_cliente = request.form['email_cliente']
+    telefone_celular = request.form['telefone_celular']
+
+    cliente = Cliente(nome_cliente,cep_cliente, rua_cliente,bairro_cliente,
+   data_nascimento_cliente,CPF_cliente,email_cliente,telefone_celular)
+    
+    cliente.save_cliente()
+
+    return redirect(url_for('clientes'))
+
