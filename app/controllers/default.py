1a031395026639ec3bfc8eeaf921e4669f325f50
Matheus Marques Silva <42191001+matheus-marques-impacta@users.noreply.github.com>
2021-09-24 19:00:56 -0300

    Bot telegram

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,34 +1,66 @@
 # -*- coding: utf-8 -*-
+
+
+
 #arquivo princpal da regra de negocio das nossas aplicacao
 #ira controlar toda nossas nossa aplicacao por meio das rotas de protocolo http
 
-from flask import render_template, request, url_for, redirect, flash, jsonify
+from flask import render_template, request, url_for, redirect, flash, jsonify,session, abort, redirect
 from app import  app, db
 from datetime import datetime, timedelta
 
 #Biblioteca para o bot do telegram
 import telebot
 
+#configurações do bot
+chave_api = "2031294195:AAFB3OtBVLEtDfAzS7SVgXuxBEnhNPFYwmY"
+bot = telebot.TeleBot(chave_api)
+
 #from app.models.tables import Pessoas
 from app.models.tables import *
 
-#configurações do bot
-chave_api = "2031294195:AAFB3OtBVLEtDfAzS7SVgXuxBEnhNPFYwmY"
+#-------------------------- login --------------------------#
+#-------------------------- login --------------------------#
+#-------------------------- login --------------------------#
 
-bot = telebot.TeleBot(chave_api)
+#google
+import os
+import pathlib
+
+import requests
+from google.oauth2 import id_token
+from google_auth_oauthlib.flow import Flow
+from pip._vendor import cachecontrol
+import google.auth.transport.requests
+
+app.secret_key = "CodeSpecialist.com"
+
+
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+GOOGLE_CLIENT_ID = "646561352344-k00r23h9lfel4s1spjpetto3bcvr57r0.apps.googleusercontent.com"
+client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
+
+flow = Flow.from_client_secrets_file(
+    client_secrets_file=client_secrets_file,
+    scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
+    redirect_uri="http://127.0.0.1:5000/callback"
+)
 
 
 #-------------------------- index --------------------------#
 #-------------------------- index --------------------------#
 #-------------------------- index --------------------------#
 
-@app.route('/')
-def login():
-    return render_template('login.html')
+#@app.route('/')
+#def login():
+#    return render_template('login.html')
 
-@app.route('/index')
-def index():
-    return render_template('index.html')
+#@app.route('/index')
+#def index():
+#    return render_template('index.html')
 
 #-------------------------- cliente --------------------------#
 #-------------------------- cliente --------------------------#
@@ -93,6 +125,66 @@ def cliente_editar_salvar():
         flash("Edicao Salva!",category="success")
 
     return redirect(url_for("clientes"))
+
+
+#-------------------------- login --------------------------#
+#-------------------------- login --------------------------#
+#-------------------------- login --------------------------#
+
+@app.route("/logar")
+def logar():
+    return render_template("login.html")
+
+@app.route("/login")
+def login():
+    authorization_url, state = flow.authorization_url()
+    session["state"] = state
+    return redirect(authorization_url)
+
+
+@app.route("/callback")
+def callback():
+    flow.fetch_token(authorization_response=request.url)
+
+    if not session["state"] == request.args["state"]:
+        abort(500)  # State does not match!
+
+    credentials = flow.credentials
+    request_session = requests.session()
+    cached_session = cachecontrol.CacheControl(request_session)
+    token_request = google.auth.transport.requests.Request(session=cached_session)
+
+    id_info = id_token.verify_oauth2_token(
+        id_token=credentials._id_token,
+        request=token_request,
+        audience=GOOGLE_CLIENT_ID
+    )
+
+    session["google_id"] = id_info.get("sub")
+    session["name"] = id_info.get("name")
+    return redirect("/protected_area")
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    #return redirect("index")
+    return redirect("logar")
+
+
+@app.route("/")
+def index():
+    #return "Hello World <a href='/login'><button>Login</button></a>"
+    return render_template("index.html")
+
+
+@app.route("/protected_area")
+#@login_is_required
+def protected_area():
+
+    #return f"Hello {session['name']}! <br/> <a href='/logout'><button>Logout</button></a>"
+    #return f"Hello {session['name']}! <br/> <a href='/logout'><button>Logout</button></a>"
+    return render_template("index.html", usuario_nome_completo=session['name'])
 
 #-------------------------- BOT --------------------------#
 #-------------------------- BOT --------------------------#
