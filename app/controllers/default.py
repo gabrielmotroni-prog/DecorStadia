02d50eca764bb30b3421c5388969b9e462f4a7ac
Gabriel Motroni <gabrielmotroni@MacBook-Pro-de-Gabriel.local>
2021-11-29 12:11:56 -0300

    ac05 correcao

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-
-
 #arquivo princpal da regra de negocio das nossas aplicacao
 #ira controlar toda nossas nossa aplicacao por meio das rotas de protocolo http
 
@@ -17,8 +15,6 @@ import os
 import requests
 import json
 
-# mercado livre
-import meli
 #boleto
 from gerencianet import Gerencianet
 
@@ -432,117 +428,3 @@ def responder(mensagem):
         bot.send_message(mensagem.chat.id, "Desculpe, não reconheço este comando")
 
 bot.polling()
-=======
-# -*- coding: utf-8 -*-
-#arquivo princpal da regra de negocio das nossas aplicacao
-#ira controlar toda nossas nossa aplicacao por meio das rotas de protocolo http
-
-from flask import render_template, request, url_for, redirect, flash, jsonify
-from app import  app, db
-from datetime import datetime, timedelta
-
-#from app.models.tables import Pessoas
-from app.models.tables import *
-
-
-
-#-------------------------- index --------------------------#
-#-------------------------- index --------------------------#
-#-------------------------- index --------------------------#
-
-@app.route('/')
-def login():
-    return render_template('login.html')
-
-@app.route('/index')
-@login_required
-def index():
-    return render_template('index.html')
-
-#-------------------------- cliente --------------------------#
-#-------------------------- cliente --------------------------#
-#-------------------------- cliente --------------------------#
-
-@app.route('/clientes')
-def clientes():
-    clientes = Cliente.list_cliente()
-
-    return render_template('clientes.html', clientes=clientes)
-
-@app.route('/cliente/cadastrar')
-def cliente_cadastrar():
-    return render_template('cliente_cadastrar.html')
-
-@app.route('/cliente/cadastrar/salvar',methods=['POST'])
-def cliente_cadastrar_salvar():
-    nome_cliente = request.form['nome_cliente']
-    cep_cliente = request.form['cep']
-    rua_cliente = request.form['endereco']
-    bairro_cliente = request.form['bairro']
-    data_nascimento_cliente = request.form['data_nascimento_cliente']
-    CPF_cliente = request.form['CPF_cliente']
-    email_cliente = request.form['email_cliente']
-    telefone_celular = request.form['telefone_celular']
-
-    cliente = Cliente(nome_cliente,cep_cliente, rua_cliente,bairro_cliente,
-   data_nascimento_cliente,CPF_cliente,email_cliente,telefone_celular)
-    
-    cliente.save_cliente()
-
-    return redirect(url_for('clientes'))
-
-
-@app.route('/cliente/editar/<int:codigo_cliente>')
-def cliente_editar(codigo_cliente=0):
-    cliente = Cliente.find_cliente(codigo_cliente)
-
-    return render_template('cliente_editar.html', cliente=cliente)
-
-
-@app.route ('/cliente/editar/salvar', methods=['POST']) 
-def cliente_editar_salvar(): 
-    #recupera campos do formulario
-    codigo_cliente= int(request.form['codigo_cliente'])
-    nome_cliente = request.form['nome_cliente']
-    email_cliente = request.form['email_cliente']
-    telefone_celular = request.form['telefone_celular']
-    CPF_cliente = request.form['CPF_cliente']
-    cep_cliente = request.form['cep']
-    rua_cliente = request.form['endereco']
-    data_nascimento_cliente = request.form['data_nascimento_cliente']
-    bairro_cliente = request.form['bairro']
-    #string to datetime object
-    data_nascimento_obj = datetime.strptime(data_nascimento_cliente,'%Y-%m-%d').date()
-    #data_nascimento_obj = datetime.utcnow()
-
-    cliente_encontrado = Cliente.find_cliente(codigo_cliente)
-    if cliente_encontrado:
-        cliente_encontrado.update_cliente(nome_cliente,email_cliente,telefone_celular,CPF_cliente,cep_cliente,rua_cliente,bairro_cliente,data_nascimento_cliente)
-        cliente_encontrado.save_cliente()
-        flash("Edicao Salva!",category="success")
-
-    return redirect(url_for("clientes"))
-
-
-#-------------------------- facebook --------------------------#
-#-------------------------- facebook --------------------------#
-#-------------------------- facebook --------------------------#
-
-@app.route('/me')
-@login_required
-def user():
-    return render_template('user.html', user=current_user)
-
-@app.route('/login-facebook')
-def login():
-    if current_user.is_anonymous():
-        return facebook.authorize(callback=url_for('facebook_authorized',
-            next=request.args.get('next') or request.referrer or None,
-            _external=True))
-    return redirect(url_for('.user'))
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('.index'))
